5dc3a5a403f833db084ca65b5ea6beac7a41658a
Nadya Dunbavo <nadya.dunbavo@mail.ru>
2024-10-16 21:37:32 +0900

    Добавляет шрифт и стили для верхней части шапки

--- sites/verstka/context/header/header-contacts.php
+++ sites/verstka/context/header/header-contacts.php
@@ -1,17 +1,9 @@
 <?php
 
-use ZLabs\BxMustache\Svg;
-
-$icon1 = new Svg();
-$icon1->src = '/local/assets/images/header/icon-phone.svg';
-
-$icon2 = new Svg();
-$icon2->src = '/local/assets/images/header/icon-mail.svg';
-
 return [
     'items' => collect([
         [
-            'icon' => $icon1,
+            'phone' => true,
             'title' => 'Отдел продаж',
             'link' => [
                 'href' => 'tel:84162528017',
@@ -19,7 +11,7 @@ return [
             ]
         ],
         [
-            'icon' => $icon2,
+            'mail' => true,
             'link' => [
                 'href' => 'mailto:blagstroy@mail.ru',
                 'text' => 'blagstroy@mail.ru'
--- sites/verstka/include/header.php
+++ sites/verstka/include/header.php
@@ -42,28 +42,32 @@ $mustache = new Mustache_Engine([
 <div class="page-inner">
     <header class="header container">
         <div class="header-top">
-            <div class="header-logo-wrap">
-                <img src="/local/assets/images/header/header-logo.svg" alt="Благовещенскстрой"
-                    width="301" height="47"
-                    class="header__logo">
+            <div class="header-top-container">
+                <div class="header-logo-wrap">
+                    <img src="/local/assets/images/header/header-logo.svg" alt="Благовещенскстрой"
+                        width="301" height="47"
+                        class="header__logo">
+                </div>
+                <?= $mustache->render('header-contacts', include $_SERVER['DOCUMENT_ROOT'] . '/context/header/header-contacts.php'); ?>
+                <a href="#" class="header-link" data-fancybox>Заказать звонок</a>
             </div>
-            <?= $mustache->render('header-contacts', include $_SERVER['DOCUMENT_ROOT'] . '/context/header/header-contacts.php'); ?>
-            <a href="#" class="header-link" data-fancybox>Заказать звонок</a>
         </div>
         <div class="header-bottom">
-            <button class="header-menu-button">
-                <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M28 17.1426V19.9997H0V17.1426H28Z" fill="#342733"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M22.4 8.57129V11.4284H0V8.57129H22.4Z" fill="#342733"/>
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M28 0V2.85714H0V0H28Z" fill="#342733"/>
-                </svg>
-            </button>
-            <div class="header-menu-content">
-<!--                --><?php //= $mustache->render('header-menu-list', include $_SERVER['DOCUMENT_ROOT'] . '/context/header/header-menu-list.php'); ?>
-                <div class="header-search">
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M14.6771 12.9094C15.6612 11.5654 16.2499 9.91462 16.2499 8.12498C16.2499 3.64502 12.6049 0 8.12494 0C3.64498 0 0 3.64502 0 8.12498C0 12.6049 3.64502 16.25 8.12498 16.25C9.91462 16.25 11.5656 15.6612 12.9096 14.677L18.2324 19.9999L20 18.2323C20 18.2323 14.6771 12.9094 14.6771 12.9094ZM8.12498 13.75C5.02318 13.75 2.50001 11.2268 2.50001 8.12498C2.50001 5.02318 5.02318 2.50001 8.12498 2.50001C11.2268 2.50001 13.75 5.02318 13.75 8.12498C13.75 11.2268 11.2267 13.75 8.12498 13.75Z" fill="#342733"/>
+            <div class="header-bottom-container">
+                <button class="header-menu-button">
+                    <svg width="28" height="20" viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M28 17.1426V19.9997H0V17.1426H28Z" fill="#342733"/>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M22.4 8.57129V11.4284H0V8.57129H22.4Z" fill="#342733"/>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M28 0V2.85714H0V0H28Z" fill="#342733"/>
                     </svg>
+                </button>
+                <div class="header-menu-content">
+    <!--                --><?php //= $mustache->render('header-menu-list', include $_SERVER['DOCUMENT_ROOT'] . '/context/header/header-menu-list.php'); ?>
+                    <div class="header-search">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M14.6771 12.9094C15.6612 11.5654 16.2499 9.91462 16.2499 8.12498C16.2499 3.64502 12.6049 0 8.12494 0C3.64498 0 0 3.64502 0 8.12498C0 12.6049 3.64502 16.25 8.12498 16.25C9.91462 16.25 11.5656 15.6612 12.9096 14.677L18.2324 19.9999L20 18.2323C20 18.2323 14.6771 12.9094 14.6771 12.9094ZM8.12498 13.75C5.02318 13.75 2.50001 11.2268 2.50001 8.12498C2.50001 5.02318 5.02318 2.50001 8.12498 2.50001C11.2268 2.50001 13.75 5.02318 13.75 8.12498C13.75 11.2268 11.2267 13.75 8.12498 13.75Z" fill="#342733"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
